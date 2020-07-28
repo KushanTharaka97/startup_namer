8edd15d9df94776a0f568d0c8d62063ed79265ea
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-28 23:55:41 +0530

    Create Build Sugesstion

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -32,7 +34,21 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    final _suggetions = <WordPair>[];
+    final _biggerFont = TextStyle(fontSize: 18.0);
   }
 }
+
+Widget _buildSuggetions() {
+  return ListView.builder(
+    padding: EdgeInsets.all(16.0),
+    itemBuilder: (context, i){
+      if(i.isOdd) return Divider();
+
+      final index = i~2;
+      if(index >= _suggetions.length){
+        _suggetions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggetions[index]);
+    })
+  }
